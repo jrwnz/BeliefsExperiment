afcbd8636f9ebea4f579c9b276365b0be55117e0
Jeremy Ward <jrwnz56@gmail.com>
2019-02-11 14:12:27 +1300

    Made order of dominance games in action section random

--- beliefs_practice/models.py
+++ beliefs_practice/models.py
@@ -27,7 +27,7 @@ class Constants(BaseConstants):
     with open('Settings/orders.json','r') as f:
         game_order = json.load(f)[name_in_url]
 
-    num_rounds = len(game_order)
+    num_rounds = len(game_order[:2])
 
 class Subsession(BaseSubsession):
     def creating_session(self):
